f61ccaab4f9eb5553d2cce59ac09fe56552dd8e6
panjibp <panji.bp.bp@gmail.com>
2021-04-16 23:51:20 +0700

    mentors endpoint in progress

---
+++ routes/mentors.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const mentorsHandler = require('./handler/mentors');
+
+router.get('/', mentorsHandler.getAll);
+router.get('/:id', mentorsHandler.get);
+router.post('/', mentorsHandler.create);
+
+module.exports = router;
